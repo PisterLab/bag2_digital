39c315761412280a55f39bb95d3cc6bafd21ca89
Lydia Lee <cs199-and@hpse-14.EECS.Berkeley.EDU>
2022-01-21 16:23:50 -0800

    [ll] systemVerilog stuff for bang-bang phase detector

--- bag2_digital/pd_bang_bang/systemVerilog/verilog.sv
+++ bag2_digital/pd_bang_bang/systemVerilog/verilog.sv
@@ -1,7 +1,7 @@
 //systemVerilog HDL for "bag2_digital", "pd_bang_bang" "systemVerilog"
 
 
-module pd_bang_bang ( D_RETIME, T, E, VDD, VSS, CLK, CLKb, D );
+module pd_bang_bang ( D_RETIME, T, E, VDD, VSS, CLK, CLKb, D, UPb, DOWN);
 
   input CLK;
   input CLKb;
@@ -10,6 +10,8 @@ module pd_bang_bang ( D_RETIME, T, E, VDD, VSS, CLK, CLKb, D );
   output wire T;
   output wire E;
   output wire D_RETIME;
+  output wire UPb;
+  output wire DOWN;
 
   inout VDD;
   inout VSS;
@@ -22,6 +24,9 @@ reg q4;
 assign D_RETIME = q4;
 assign T = ~(q1 | q2);
 assign E = ~(q2 | q4);
+
+assign UPb = ~T | E;
+assign DOWN = T & E;
 
 always @(posedge CLK) begin
 	q1 <= D;
